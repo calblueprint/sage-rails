ba10296e8445aba57134606c200bfa3fcc3297a2
Charles Xue <charlesx2013@gmail.com>
2015-09-30 03:36:05 -0700

    changed column name in sessions to finish

--- app/models/session.rb
+++ app/models/session.rb
@@ -6,10 +6,13 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  start      :datetime         not null
-#  end        :datetime         not null
+#  finish     :datetime         not null
 #  school_id  :integer
 #  user_id    :integer
 #
 
 class Session < ActiveRecord::Base
+  def calculate_time
+
+  end
 end
