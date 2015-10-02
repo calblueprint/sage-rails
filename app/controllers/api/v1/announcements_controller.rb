cc92db50fd7ae8dab8edbb03a16206a2e4c01fa3
Charles Xue <charlesx2013@gmail.com>
2015-10-01 17:44:38 -0700

    finished announcement controller

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -2,18 +2,35 @@ class Api::V1::AnnouncmentsController < Api::V1::BaseController
   load_and_authorize_resource param_method: :announcements_params
 
   def index
+    render json: @announcements, each_serializer: AnnouncementListSerializer
   end
 
   def show
+    render json: @announcement, serializer: AnnouncementSerializer
   end
 
   def create
+    if @announcement.save
+      render json: @announcement, serializer: AnnouncementSerializer
+    else
+      error_response(@announcement)
+    end
   end
 
   def update
+    if @announcement.update_attributes(announcements_params)
+      render json: @announcement, serializer: AnnouncementSerializer
+    else
+      error_response(@announcement)
+    end
   end
 
   def destroy
+    if @announcement.destroy
+      render json: @announcement, serializer: AnnouncementSerializer
+    else
+      error_response(@announcement)
+    end
   end
 
   private
