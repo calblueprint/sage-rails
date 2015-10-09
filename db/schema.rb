ee295e3709b4fd1edc820e213fe8ef34386d89a4
Charles Xue <charlesx2013@gmail.com>
2015-10-08 18:13:32 -0700

    added migration to add year and season to semesters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008032650) do
+ActiveRecord::Schema.define(version: 20151008034310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20151008032650) do
     t.integer  "year"
     t.integer  "season"
   end
+
+  add_index "semesters", ["year", "season"], name: "index_semesters_on_year_and_season", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                             null: false
