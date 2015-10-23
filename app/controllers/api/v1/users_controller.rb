4fa3487f4a4963636386a5623c4ee9ff116b9c35
Charles Xue <charlesx2013@gmail.com>
2015-10-23 02:17:39 -0700

    added controller methods for images

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
   # If we're creating a user, we want to skip the user validations
+  prepend_before_filter :convert_base64_to_images, only: [:create]
   skip_before_filter :authenticate_user_from_token!, only: [:create]
   skip_before_filter :authenticate_api_v1_user!,     only: [:create]
 
@@ -46,6 +47,14 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :role,
-                                 :password, :current_password, :password_confirmation)
+                                 :password, :current_password, :password_confirmation, :image)
+  end
+
+  def convert_base64_to_images
+    # TODO(mark): Consider making this a helper function for all models
+    unless params[:user][:data].blank?
+        params[:user][:image] = Photo.convert_base64(params[:user][:data])
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :school_id, presence: true
+  validates :image, presence: true
 
   # Relationships
   has_many :check_ins
