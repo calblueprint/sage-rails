cd3d67074295f298c593ba2ad69d0a49dc7b5abd
Charles Xue <charlesx2013@gmail.com>
2015-10-23 19:55:38 -0700

    removed user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,14 +87,4 @@ class User < ActiveRecord::Base
   def image_tmp_url
     "/tmp/uploads/#{image_tmp}" unless image_tmp.nil?
   end
-
-  def self.convert_base64(data)
-    return unless data
-
-    temp_file = Tempfile.new [Devise.friendly_token, "jpg"]
-    temp_file.binmode
-    temp_file.write(Base64.decode64(data))
-
-    ActionDispatch::Http::UploadedFile.new(tempfile: temp_file, filename: "#{Devise.friendly_token}.jpg")
-  end
 end
