f6dd61d177652b189f581c955e17d6704149100b
Charles Xue <charlesx2013@gmail.com>
2015-10-31 18:30:16 -0700

    convert utc to pst

--- app/serializers/check_ins/base_check_in_serializer.rb
+++ app/serializers/check_ins/base_check_in_serializer.rb
@@ -1,3 +1,11 @@
 class BaseCheckInSerializer < ActiveModel::Serializer
   attributes :start, :finish, :verified, :comment, :school_id, :user_id
+
+  def start
+    object.start.in_time_zone("Pacific Time (US & Canada)")
+  end
+
+  def finish
+    object.finish .in_time_zone("Pacific Time (US & Canada)")
+  end
 end
