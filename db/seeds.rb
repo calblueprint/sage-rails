6852d9022eac4c0490c3aefadc15d5291f0aa84b
Charles Xue <charlesx2013@gmail.com>
2015-11-04 20:28:54 -0800

    fixed verified student

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ def create_verified_students
                 email: "vstudent#{n}@berkeley.edu",
                 password: 'password',
                 role: 0,
-                school_id: n,
+                school_id: 1,
                 volunteer_type: 1,
                 verified: true
     puts "Created verified student #{n}"
@@ -55,7 +55,7 @@ def create_unverified_students
                 email: "uvstudent#{n}@berkeley.edu",
                 password: 'password',
                 role: 0,
-                school_id: n,
+                school_id: 1,
                 volunteer_type: 1,
                 verified: false
     puts "Created student #{n}"
