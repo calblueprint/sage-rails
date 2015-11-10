87cbb7f1c2dfef944160cb2c0d5fc4833da5989a
Charles Xue <charlesx2013@gmail.com>
2015-11-09 21:50:55 -0800

    move to pst

--- app/serializers/announcements/base_announcement_serializer.rb
+++ app/serializers/announcements/base_announcement_serializer.rb
@@ -1,6 +1,10 @@
 class BaseAnnouncementSerializer < ActiveModel::Serializer
   attributes :school_id, :user_id, :title, :body, :created_at, :user_name, :school_name
 
+  def created_at
+    object.created_at.in_time_zone("Pacific Time (US & Canada)")
+  end
+
   def user_name
     object.user.name
   end
