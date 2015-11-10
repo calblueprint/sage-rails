34670d70539e7df8633957761c3773eb5ba6eec1
Charles Xue <charlesx2013@gmail.com>
2015-11-09 21:27:48 -0800

    added name to announcement

--- app/serializers/announcements/base_announcement_serializer.rb
+++ app/serializers/announcements/base_announcement_serializer.rb
@@ -1,3 +1,7 @@
 class BaseAnnouncementSerializer < ActiveModel::Serializer
-  attributes :school_id, :user_id, :title, :body
+  attributes :school_id, :user_id, :title, :body, :created_at, :name
+
+  def name
+    object.user.name
+  end
 end
