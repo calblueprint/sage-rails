384032faacaba0d4b888812a07f19f3f187419ec
Charles Xue <charlesx2013@gmail.com>
2015-11-29 21:31:49 -0800

    list of users doesn't include director

--- app/serializers/schools/school_serializer.rb
+++ app/serializers/schools/school_serializer.rb
@@ -1,4 +1,8 @@
 class SchoolSerializer < BaseSchoolSerializer
   has_many :users, serializer: UserListSerializer
   has_one :director, serializer: UserListSerializer
+
+  def users
+    object.users.director_id(nil)
+  end
 end
