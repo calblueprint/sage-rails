2662dd7100361035c0345733a691a95d39d92664
Charles Xue <charlesx2013@gmail.com>
2015-12-04 02:55:03 -0800

    add some semester validations

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -17,9 +17,24 @@ class Semester < ActiveRecord::Base
   validates :finish, presence: true, date: { after: :start }
   validates :year, presence: true, numericality: { only_integer: true, greater_than: 2014 }
   validates :season, presence: true, numericality: { only_integer: true }
+  validate :has_no_overlap
 
   validates_uniqueness_of :year, scope: :season
 
   # Scopes
   scope :by_period, -> year, season { where(year: year, season: season) }
+
+  def self.by_date(date)
+    find_by('start <= ? AND finish >= ?', date, date)
+  end
+
+  def has_no_overlap
+    unless Semester.by_date(start)
+      errors.add(:start, "has overlapping semester")
+    end
+
+    unless Semester.by_date(finish)
+      errors.add(:finish, "has overlapping semester")
+    end
+  end
 end
