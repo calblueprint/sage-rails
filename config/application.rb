e9e9794b209f7389acc1e0941748b42a943422ad
Charles Xue <charlesx2013@gmail.com>
2015-12-09 03:15:17 -0800

    set default time zone to pst

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,8 @@ module SageRails
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.active_record.default_timezone = :local
 
     config.time_zone = 'Pacific Time (US & Canada)'
     config.active_record.default_timezone = :local
