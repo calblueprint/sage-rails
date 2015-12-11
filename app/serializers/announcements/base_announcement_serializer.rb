8d79bf206aa9bab2a4b54d60c787b028c07b7f47
Charles Xue <charlesx2013@gmail.com>
2015-12-10 16:06:51 -0800

    added category

--- app/serializers/announcements/base_announcement_serializer.rb
+++ app/serializers/announcements/base_announcement_serializer.rb
@@ -1,3 +1,7 @@
 class BaseAnnouncementSerializer < ActiveModel::Serializer
-  attributes :id, :school_id, :user_id, :title, :body, :created_at
+  attributes :id, :school_id, :user_id, :title, :body, :created_at, :category
+
+  def category
+    object[:category]
+  end
 end
