7bc7d8756e66d76b2b581d6633b48887474c0286
Charles Xue <charlesx2013@gmail.com>
2016-01-17 18:12:30 -0800

    adding user semester serializers

--- app/serializers/users/user_serializer.rb
+++ app/serializers/users/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < BaseUserSerializer
-  has_one :school, serializer: SchoolListSerializer
-  has_one :current_semester, serializer: UserSemesterSerializer
+  has_one :school, serializer: SchoolNameSerializer
+  has_one :current_semester, serializer: UserSemesterListSerializer
 
   def current_semester
     semester = Semester.current_semester.first
