30d523235e6749bd8db4ae6efe8535b1d0654f17
Charles Xue <charlesx2013@gmail.com>
2016-01-26 15:27:52 -0800

    fixing serializatino params

--- app/serializers/users/user_serializer.rb
+++ app/serializers/users/user_serializer.rb
@@ -3,7 +3,8 @@ class UserSerializer < BaseUserSerializer
   has_many :check_ins, each_serializer: CheckInSerializer
 
   def check_ins
-    return [] unless serialization_options[:params][:semester_id] &&
+    return [] unless serialization_options[:params] &&
+                     serialization_options[:params][:semester_id] &&
                      serialization_options[:params][:check_ins]
 
     CheckIn.where(semester_id: serialization_options[:params][:semester_id])
