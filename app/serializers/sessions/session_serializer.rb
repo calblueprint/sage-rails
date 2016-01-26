472c195b76db210bd2556c9a8de1ff48e39d2a88
Charles Xue <charlesx2013@gmail.com>
2016-01-26 03:16:17 -0800

    remove user serializer from session serializer

--- app/serializers/sessions/session_serializer.rb
+++ app/serializers/sessions/session_serializer.rb
@@ -1,7 +1,6 @@
 class SessionSerializer < BaseSessionSerializer
   has_one :user, serializer: UserSerializer
   has_one :school, serializer: SchoolSessionSerializer
-  has_one :user_semester, serializer: UserSemesterSerializer
   has_one :current_semester, serializer: SemesterListSerializer
 
   def user
@@ -10,12 +9,6 @@ class SessionSerializer < BaseSessionSerializer
 
   def school
     object.school
-  end
-
-  def user_semester
-    semester = Semester.current_semester.first
-    return nil unless semester
-    UserSemester.find_by(user_id: object.id, semester_id: semester.id)
   end
 
   def current_semester
