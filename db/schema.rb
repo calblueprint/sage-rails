c22bf9dc8e88d2062c569e225b5ab75683003ea0
Charles Xue <charlesx2013@gmail.com>
2016-02-20 17:03:41 -0800

    Summary: Added radius of school to model
    Test Plan:
    
    Reviewers:
    
    Subscribers: samz, kelseylam, baerica, amillman
    
    Projects: #Sage-Rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109121430) do
+ActiveRecord::Schema.define(version: 20160221005945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160109121430) do
     t.decimal  "lat",        precision: 10, scale: 7, default: 0.0
     t.decimal  "lng",        precision: 10, scale: 7, default: 0.0
     t.string   "address"
+    t.integer  "radius",                              default: 200
   end
 
   create_table "semesters", force: :cascade do |t|
