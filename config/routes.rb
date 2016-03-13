d474f05d26669ba05e1dc9c81d86df135c34d53d
Charles Xue <charlesx2013@gmail.com>
2016-03-12 17:32:22 -0800

    Adding in registration route to register device id and device type

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ Rails.application.routes.draw do
       resources :users,          except: [:new, :edit] do
         member do
           get :state
+          post :register
         end
 
         collection do
