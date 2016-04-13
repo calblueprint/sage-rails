05ce25a2b0fd7ce01feb8bd3e6da776bf12348f1
Charles Xue <charlesx2013@gmail.com>
2016-04-13 12:15:38 -0700

    take out permissions for now

--- app/models/abilities/user_ability.rb
+++ app/models/abilities/user_ability.rb
@@ -4,15 +4,13 @@ class UserAbility
   def initialize(user)
     user ||= User.new
 
-    can :manage, User, id: user.id
-    can :manage, CheckIn, user_id: user.id
-    can :manage, UserSemester, user_id: user.id
-    can :read, Semester
-    can :read, Announcement
-    can :read, School
+    can :manage, :all
 
-    if user.admin? || user.president?
-      can :manage, :admin_dashboard
-    end
+    # can :manage, User, id: user.id
+    # can :manage, CheckIn, user_id: user.id
+    # can :manage, UserSemester, user_id: user.id
+    # can :read, Semester
+    # can :read, Announcement
+    # can :read, School
   end
 end
