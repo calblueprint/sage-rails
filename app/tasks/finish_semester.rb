bb02baecf5ff2e62478b9b947895475a4d6d4b3f
Charles Xue <charlesx2013@gmail.com>
2016-07-01 20:29:32 -0700

    Fixing tests, refactoring pause

--- app/tasks/finish_semester.rb
+++ app/tasks/finish_semester.rb
@@ -2,7 +2,7 @@ class FinishSemester
   def initialize(semester)
     @semester = semester
     @hours_map = User::REQ_HOURS.map do |k, v|
-      [k.to_s, v * get_num_weeks(semester)]
+      [k.to_s, v * semester.weeks_completed]
     end.to_h
   end
 
@@ -11,10 +11,6 @@ class FinishSemester
       next unless u.user.school_id
       u.update_attribute(:completed, has_completed_semester?(u))
     end
-  end
-
-  def get_num_weeks(semester)
-    (semester.finish - semester.start).ceil / 60 / 60 / 24 / 7
   end
 
   def has_completed_semester?(user_semester)
--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -17,7 +17,7 @@ require 'rails_helper'
 
 describe Semester do
   describe 'should not be valid' do
-    describe 'if there is' do
+    describe 'if there' do
       let!(:semester) do
         create :semester, start: Time.now - 1.week, finish: nil
       end
@@ -26,18 +26,18 @@ describe Semester do
         build :semester, start: Time.now, finish: nil
       end
 
-      it 'a current semester' do
+      it 'is a current semester' do
         expect(invalid_semester.valid?).to be false
       end
     end
 
     describe 'if there is no current_semester' do
       let!(:current_semester) do
-        create :semester, start: Time.now, finish: Time.now + 1.week
+        create :semester, start: Time.now - 1.week, finish: Time.now
       end
 
       let!(:invalid_semester) do
-        build :semester, start: Time.now, finish: nil
+        build :semester, start: Time.now - 2.days, finish: nil
       end
 
       it 'but it overlaps' do
@@ -59,7 +59,7 @@ describe Semester do
   describe 'should be valid' do
     describe "if it has" do
       let!(:semester) do
-        build :semester, start: Time.now, finish: nil
+        build :semester, start: Time.now - 1.week, finish: nil
       end
 
       it "a valid start date" do
@@ -68,7 +68,7 @@ describe Semester do
 
       it "a valid finish date" do
         semester.save
-        expect(semester.update_attributes({ finish: Time.now + 1.week })).to be true
+        expect(semester.update_attributes({ finish: Time.now })).to be true
       end
     end
   end
