1e6baf57a3bded15656d622786b8d6555ecc48f7
Charles Xue <charlesx2013@gmail.com>
2016-07-10 19:49:28 -0700

    Fix some pause bugs

--- app/serializers/semesters/base_semester_serializer.rb
+++ app/serializers/semesters/base_semester_serializer.rb
@@ -1,5 +1,5 @@
 class BaseSemesterSerializer < ActiveModel::Serializer
-  attributes :id, :start, :finish, :season
+  attributes :id, :start, :finish, :season, :paused, :date_paused
 
   def season
     object[:season]
