a4b566b97272fe8e95908d62f46f4a9c2a26803f
Charles Xue <charlesx2013@gmail.com>
2016-08-19 18:06:19 -0700

    Refining and testing user abilities

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,4 +10,35 @@ FactoryGirl.define do
     finish 1.year.ago + 2.weeks
     season 0
   end
+
+  factory :school do
+    name "School"
+    lat 0.0
+    lng 0.0
+    address "Address"
+    radius 50
+  end
+
+  factory :user do
+    email { generate(:email) }
+    verified true
+    first_name 'First'
+    last_name 'Last'
+    volunteer_type 0
+    password 'password'
+
+    school
+
+    factory :director do
+     role 1
+    end
+
+    factory :president do
+      role 2
+    end
+  end
+
+  sequence :email do |n|
+    "email#{n}@berkeley.edu"
+  end
 end
