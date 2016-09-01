30b2bf2423f39670fe926a5ce7de11376b52941f
Charles Xue <charlesx2013@gmail.com>
2016-09-01 02:09:35 -0700

    Set semester active task

--- app/models/semester_task.rb
+++ app/models/semester_task.rb
@@ -5,9 +5,10 @@
 #  id             :integer          not null, primary key
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
-#  set_active     :boolean
-#  increment_week :boolean
+#  set_active     :boolean          default(FALSE)
+#  increment_week :boolean          default(FALSE)
 #
 
 class SemesterTask < ActiveRecord::Base
+  scope :between, -> start, finish { where('? <= created_at AND created_at <= ?', start, finish) }
 end
--- app/tasks/finish_semester.rb
+++ app/tasks/finish_semester.rb
@@ -1,16 +1,26 @@
 class FinishSemester
   def initialize(semester)
     @semester = semester
+
     @hours_map = User::REQ_HOURS.map do |k, v|
       [k.to_s, v * semester.weeks_completed]
     end.to_h
   end
 
   def perform
+    Rails.logger.info("Starting finish semester")
+
+    unless @semester && @semester.finish
+      Rails.logger.info("Not a valid semester")
+      return
+    end
+
     @semester.user_semesters.each do |u|
       next unless u.user.school_id
       u.update_attribute(:completed, has_completed_semester?(u))
     end
+
+    Rails.logger.info("Finished finish semester")
   end
 
   def has_completed_semester?(user_semester)
